8835795579b14ecd8b02e63869033d1cede21668
PEDRO HENRIQUE OLIVEIRA SILVA <pedro.hosilva30@senacsp.edu.br>
2024-10-21 16:22:00 -0300

    v.  1.0

---
+++ backup.bat
@@ -0,0 +1,5 @@
+mkdir backup
+copy /windows/*.txt /users/pedro.hosilva30/documents/script-backup/backup
+cd system32
+copy windows/system32/*.log /users/pedro.hosilva30/documents/script-backup/backup
+copy windows/system32/*.txt /users/pedro.hosilva30/documents/script-backup/backup
